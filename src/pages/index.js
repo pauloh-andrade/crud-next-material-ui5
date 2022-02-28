ce42ef0f45168524108f4ff70221d780a478e8d5
Paulo Andrade <paulo.andrade@accurate.com.br>
2022-02-28 08:55:42 -0300

    feat: adicionado tema claro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
+import { Button, ThemeProvider } from "@mui/material";
+import { LightTheme } from "../shared/themes";
+
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider theme={LightTheme}>
+      <Button variant="contained" color="primary">
+        Teste
+      </Button>
+    </ThemeProvider>
   )
-}
+};
+
 export default Home;
