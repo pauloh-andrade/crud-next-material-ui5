9d1c223611425052c513474a656d78f7d6a872ab
Paulo Andrade <paulo.andrade@accurate.com.br>
2022-03-14 11:32:47 -0300

    feat: Atributos dos botões de ferramentas de detalhe

--- src/shared/componnents/ferramentasDeDetalhe/FerramentasDeDetalhe.jsx
+++ src/shared/componnents/ferramentasDeDetalhe/FerramentasDeDetalhe.jsx
@@ -1,6 +1,19 @@
 import { Box, useTheme, Paper, Button, Icon, Divider } from "@mui/material";
 
-export const FerramentasDeDetalhe = () =>{
+export const FerramentasDeDetalhe = ({
+    textoBotaoNovo = "Novo",
+    mostrarBotaoNovo = true,
+    mostrarBotaoApagar= true,
+    mostarBotaoSalvar= true,
+    mostrarBotaoSalvarEVoltar = false,
+    mostrarBotaoVoltar = true,
+
+    aoClicarEmNovo,
+    aoClicarEmSalvar,
+    aoClicarEmSalvarEVoltar,
+    aoClicarEmApagar,
+    aoClicarEmVoltar
+}) =>{
     const theme = useTheme();
     return(
         <Box 
@@ -13,47 +26,58 @@ export const FerramentasDeDetalhe = () =>{
         alignItems="center"
         component={Paper}
         >
-           <Button 
-                color="primary" 
-                variant="contained" 
-                startIcon={<Icon>save</Icon>} 
-                disableElevation
-                >
-                Salvar
-            </Button>
-            <Button 
-                color="primary" 
-                variant="outlined" 
-                startIcon={<Icon>save</Icon>} 
-                disableElevation
-                >
-                Salvar e voltar
-            </Button>
-            <Button 
-                color="primary" 
-                variant="outlined" 
-                startIcon={<Icon>delete</Icon>} 
-                disableElevation
-                >
-                Apagar
-            </Button>
-            <Button 
-                color="primary" 
-                variant="outlined" 
-                startIcon={<Icon>add</Icon>} 
-                disableElevation
-                >
-                Novo
-            </Button>
+            {mostarBotaoSalvar && (
+                <Button 
+                    color="primary" 
+                    variant="contained" 
+                    startIcon={<Icon>save</Icon>} 
+                    disableElevation
+                    >
+                    Salvar
+                </Button>
+            )}
+            {mostrarBotaoSalvarEVoltar && (
+                <Button 
+                    color="primary" 
+                    variant="outlined" 
+                    startIcon={<Icon>save</Icon>} 
+                    disableElevation
+                    >
+                    Salvar e voltar
+                </Button>
+            )}
+            {mostrarBotaoApagar && (
+                <Button 
+                    color="primary" 
+                    variant="outlined" 
+                    startIcon={<Icon>delete</Icon>} 
+                    disableElevation
+                    >
+                    Apagar
+                </Button>
+            )}
+            {mostrarBotaoNovo && (
+                <Button 
+                    color="primary" 
+                    variant="outlined" 
+                    startIcon={<Icon>add</Icon>} 
+                    disableElevation
+                    >
+                    {textoBotaoNovo}
+                </Button>
+            )}
+            
             <Divider variant="middle" orientation="vertical" />
-            <Button 
-                color="primary" 
-                variant="outlined" 
-                startIcon={<Icon>arrow_back</Icon>} 
-                disableElevation
-                >
-                voltar
-            </Button>
+            {mostrarBotaoVoltar && (
+                <Button 
+                    color="primary" 
+                    variant="outlined" 
+                    startIcon={<Icon>arrow_back</Icon>} 
+                    disableElevation
+                    >
+                    voltar
+                </Button>
+            )}
         </Box>
     );
 };
