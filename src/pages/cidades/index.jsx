aabff79e270cca65a40cb81bbf4ae922d9a957f6
Paulo Henrique <pauloh8755@gmail.com>
2022-05-06 11:34:26 -0300

    feat: filter

--- src/pages/cidades/index.jsx
+++ src/pages/cidades/index.jsx
@@ -1,11 +1,41 @@
+import { useRouter } from 'next/router';
+
+import { useEffect, useMemo, useState } from 'react';
 import { FerramentasDaListagem } from '../../componnents';
 import LayoutBase from '../../layout/LayoutBase';
 
 const ListagemDeCidade = () => {
+	const router = useRouter();
+
+	const [searchParams, setSearchParams] = useState(router.query);
+
+	useEffect(() => {
+		// Always do navigations after the first render/cidades/? busca=${search}
+		router.push({ pathname: '/cidades', query: { busca: search } }, undefined, { shallow: true });
+		console.log(router.query);
+	}, [search]);
+
+	const busca = useMemo(() => {
+		const { busca } = router.query;
+		return busca;
+	});
+
+	// useEffect(() => {
+	// 	router.push(`/cidades/?aaa=aaaa`, undefined, { shallow: true });
+	// }, []);
+
 	return (
 		<LayoutBase
 			titulo="Controle de cidades"
-			barraDeFerramentas={<FerramentasDaListagem mostrarInputBusca textoBotaoNovo="nova" mostrarBotaoSalvarEVoltar />}>
+			barraDeFerramentas={
+				<FerramentasDaListagem
+					mostrarInputBusca
+					textoBusca={search}
+					aoMudarTextoDeBusca={e => setSearchParams({ ...searchParams, e })}
+					textoBotaoNovo="nova"
+					mostrarBotaoSalvarEVoltar
+				/>
+			}>
 			testeando
 		</LayoutBase>
 	);
